85fb1303747a8bf861e3ad36011ef2b4a35e14fe
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-08-01 14:37:59 +0100

    added colour expenation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {task: "Wash dishes", priority: "low"},
+    {task: "Cook meal", priority: "high"}
+  ]);
+
+  const [newTask, setNewTask] = useState("")
+
+  const [newPriorty, setNewPriorty] = useState()
+
+  const tasksNodes = tasks.map( (task, index) => {
+    return (
+      <li key={index} className={task.priority == 'high' ? 'high' : 'low'}>
+        <span>
+          {task.task}
+        </span>
+      </li>
+    )
+  })
+
+  const handelTaskInput = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const handelPriortyInput = (event) => {
+      setNewPriorty(event.target.value)
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault()
+    const copyTasks = [...tasks]
+    copyTasks.push({
+      task: newTask,
+      priority: newPriorty
+    })
+    setTasks(copyTasks)
+    setNewTask("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>To do List</h1>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor='new-task'>Add new task:</label>
+        <input id='new-task' type='text' value={newTask} onChange={handelTaskInput}/>
+        <div onChange={handelPriortyInput}>
+          <input id='low' type='radio' value='low' name='priorty' required/>
+          <label htmlFor='low'>Low</label>
+          <input id='high' type='radio' value='high' name='priorty' required/>
+          <label htmlFor='high'>High</label>
+        </div>
+        <button type='submit' value='save-new-task'>Submit</button>
+      </form>
+
+      <ul>
+        <p>Low priority tasks are green, high priority tasks are orange</p>
+        {tasksNodes}
+      </ul>
+
+    </>
   );
 }
 
